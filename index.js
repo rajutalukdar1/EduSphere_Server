e21b7280faa58ea4d7a9c8facb9cdc1ee2d2c1eb
rajutalukdar1 <lalutaluker@gmail.com>
2022-11-08 20:42:06 +0600

    home page data lodded

--- index.js
+++ index.js
@@ -14,12 +14,19 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d64dkmr.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
+        const serviceCollection = client.db('EduSphere').collection('services');
 
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service);
+        });
     }
     finally {
 
