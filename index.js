6d770d9f7e1a83633b26ba9019a3021ed4879fea
rajutalukdar1 <lalutaluker@gmail.com>
2022-11-09 02:54:55 +0600

    lemet data get done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,12 +21,25 @@ async function run() {
     try {
         const serviceCollection = client.db('EduSphere').collection('services');
 
+        app.get('/service', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.limit(3).toArray();
+            res.send(service);
+        });
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const service = await cursor.toArray();
             res.send(service);
         });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query)
+            res.send(service);
+        })
     }
     finally {
 
