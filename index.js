3a886c03b9f5d5ddfbd227ae455f8d5f757ec8f2
rajutalukdar1 <lalutaluker@gmail.com>
2022-11-10 23:58:23 +0600

    all done

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 async function run() {
     try {
         const serviceCollection = client.db('EduSphere').collection('services');
@@ -50,17 +66,35 @@ async function run() {
             res.send(service);
         })
 
-        app.get('/reviews', async (req, res) => {
+        // app.get('/reviews', verifyJWT, async (req, res) => {
+        //     // const decoded = req.decoded;
+        //     // console.log('inside log', decoded);
+        //     // if (decoded.email !== req.query.email) {
+        //     //     res.status(403).send({ message: 'unauthorized access' });
+        //     // }
+        //     let query = {}
+        //     if (req.query.service) {
+        //         query = {
+        //             service: req.query.service
+        //         }
+        //     };
+        //     const cursor = reviewCollection.find(query)
+        //     const orders = await cursor.toArray();
+        //     res.send(orders)
+        // });
+
+
+        app.get('/myReviews', async (req, res) => {
             let query = {}
-            if (req.query.service) {
+            if (req.query.email) {
                 query = {
-                    service: req.query.service
+                    email: req.query.email
                 }
             };
             const cursor = reviewCollection.find(query)
             const orders = await cursor.toArray();
             res.send(orders)
-        });
+        })
 
 
         app.post('/reviews', async (req, res) => {
@@ -82,7 +116,14 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const service = await cursor.toArray();
             res.send(service);
+
         });
+        // app.delete('/myReviews/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await OrderCollection.deleteOne(query);
+        //     res.send(result);
+        // })
     }
     finally {
 
